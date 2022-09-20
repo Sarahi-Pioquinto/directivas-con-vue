27097159fc63b0018d7944a689b1c73ffb8b4ddc
Sarah López <a3521110542@alumno.uttehuacan.edu.mx>
2022-09-20 13:38:20 -0500

    directivas con vue

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h1>Directivas</h1>
+    <hr/>
+    
+
+    <span v-text="titulo" ></span>
+    <h1>{{titulo}}</h1>
+    <hr/>
+    <h1>v-show</h1>
+    <h3>tu edad es : {{edad}}</h3>
+    <h2 v-if="edad>=18 && edad >0 && edad<100" >mayor de edad</h2>
+    <h2 v-else-if="edad<18 && edad >0 && edad<100" >menor de edad</h2>
+    <h2 v-else >Edad invalida</h2>
+
+
+
+    <button @click="inc()">incrementar</button>
+    <button @click="dis()">disminuir</button>
+
+
+    <div v-html="html" ></div>
+    <hr/>
+   
+
+
+  </div>
+</template>
+
+<script>
+  export default{
+    data: () => ({
+      titulo: 'Hola desede vue3',
+      html: "<h3>Soy un H3</h3>",
+      edad:0,
+
+    }),
+    methods:{
+      inc(){
+        this.edad++;
+      },
+      dis(){
+        this.edad--;
+      }
+    },
+  };
+</script>
+<style scoped>
+  div{
+    text-align: center;
+    margin: 0px 2rem;
+  }
+  h2{
+    color: brown;
+  }
+</style>
