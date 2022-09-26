7c455f1f3b411a12989520a99e08dde923946220
Sarah López <a3521110542@alumno.uttehuacan.edu.mx>
2022-09-26 11:01:32 -0500

    Segunda version

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,20 @@
     <h1>Directivas</h1>
     <hr/>
     
-
+    <h1>v-text</h1>
     <span v-text="titulo" ></span>
     <h1>{{titulo}}</h1>
     <hr/>
+
+
     <h1>v-show</h1>
+    <h3>tu edad es : {{edad}}</h3>
+    <h2 v-show="edad>=18" >Mayor de edad</h2>
+    <h2 v-show="edad<18" >Menor de edad</h2>
+    <hr/>
+
+    
+    <h1>v-if v-else-if vi-else</h1>
     <h3>tu edad es : {{edad}}</h3>
     <h2 v-if="edad>=18 && edad >0 && edad<100" >mayor de edad</h2>
     <h2 v-else-if="edad<18 && edad >0 && edad<100" >menor de edad</h2>
@@ -18,7 +27,7 @@
     <button @click="inc()">incrementar</button>
     <button @click="dis()">disminuir</button>
 
-
+    <h1>v-html</h1>
     <div v-html="html" ></div>
     <hr/>
    
